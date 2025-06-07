a60619dc30085eaf8230231e920e964960fe3e05
Mervyn <vip126@vip.qq.com>
2025-06-08 02:06:09 +0800

    移除不再使用的 desktop_window 和 path_provider 相关依赖，更新 pubspec.yaml 和各平台的插件注册文件，优化项目结构。

--- linux/flutter/generated_plugins.cmake
+++ linux/flutter/generated_plugins.cmake
@@ -3,7 +3,6 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  desktop_window
   media_kit_libs_linux
   media_kit_video
   volume_controller
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,7 +3,6 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  desktop_window
   media_kit_libs_windows_video
   media_kit_video
   volume_controller
